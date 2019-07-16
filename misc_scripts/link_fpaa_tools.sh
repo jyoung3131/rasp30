118035e6f6bbb0a16e188fbbe0066bf8ac611382
Jeff Young <jyoung9@gatech.edu>
2019-07-16 13:42:33 -0400

    Updated scripts for remote usage

--- misc_scripts/link_fpaa_tools.sh
+++ misc_scripts/link_fpaa_tools.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 #Last updated: 07/16/2019
 #This script will link the RASP tools and appropriate scilab installation to your home directory.
-#NOTE: Run this from your home directory!
+#NOTE: Run this from your home directory or wherever you have set as FPAAHOME!
 
 # exit on error
 set -e
 
 link_tools() {
-	ln -s /tools/neuro/fpaa/x86_64/scilab-5.4.1 scilab-5.4.1
-	ln -s /tools/neuro/fpaa/x86_64/scilab-5.5.2 scilab-5.5.2
-	ln -s /opt/fpaa/rasp30 rasp30
+	ln -s /tools/neuro/fpaa/x86_64/scilab-5.4.1 $FPAAHOME/scilab-5.4.1
+	ln -s /tools/neuro/fpaa/x86_64/scilab-5.5.2 $FPAAHOME/scilab-5.5.2
+	ln -s /opt/fpaa/rasp30 $FPAAHOME/rasp30
 }
 
 #If you want to unlink this software you can use the following function
 unlink_tools() {
-	unlink rasp30
-	unlink scilab-5.4.1
-	unlink scilab-5.5.2
+	unlink ${FPAAHOME}/rasp30
+	unlink ${FPAAHOME}/scilab-5.4.1
+	unlink ${FPAAHOME}/scilab-5.5.2
 }
 
+echo "Linking tools at FPAAHOME: $FPAAHOME"
 link_tools
+
+#Uncomment to uninstall links to tools
 #unlink_tools
--- misc_scripts/set_fpaa_paths.sh
+++ misc_scripts/set_fpaa_paths.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #Last updated: 07/16/2019
 #This script will set the path to your RASP installation and Scilab installation.
+#Run using ". set_fpaa_paths.sh" to source this script.
 
 #Specify which version of Scilab is being used
 export SCILAB_VERSION=scilab-5.4.1
 #export SCILAB_VERSION=scilab-5.5.2
+echo "Using Scilab version: $SCILAB_VERSION"
 
 #Specify where the rasp30 and Scilab tools are linked to - usually your home directory
 export FPAAHOME=~/
+echo "FPAAHOME is located at $FPAAHOME"
