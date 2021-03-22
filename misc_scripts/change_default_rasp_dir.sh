206dfe803d0de38c6717f443a696409d30cd38f4
Jeffrey Young <jyoung3131@users.noreply.github.com>
2021-03-21 20:13:30 -0400

    Update change_default_rasp_dir.sh

--- misc_scripts/change_default_rasp_dir.sh
+++ misc_scripts/change_default_rasp_dir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Last updated: 07/16/2019
-#This script searches through the RASP tools and replaces $FPAAHOME with an environment variable that 
-#can be set by the user.
+#This script searches through the RASP tools and replaces $FPAAHOME with an environment variable, FPAAHOME, that 
+#can be set by the user at runtime.
 
 find . -type f -exec sed -i 's/\/home\/ubuntu/$FPAAHOME/g' {} +
